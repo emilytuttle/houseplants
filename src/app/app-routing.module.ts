aee942ab4daba0dad9b4f0664ee5a01678cf32c9
Emily Tuttle <emilytuttle00@gmail.com>
2025-04-07 16:34:14 -0600

    finished proj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PlantListComponent } from './plant-list/plant-list.component';
 import { PlantDetailComponent } from './plant-detail/plant-detail.component';
+import { EditPlantComponent } from './edit-plant/edit-plant.component';
 
 export const routes: Routes = [
   { path: '', component: PlantListComponent },
   { path: 'plants', component: PlantListComponent },
   { path: 'plants/:id', component: PlantDetailComponent },
+  { path: 'plants/:id/edit', component: EditPlantComponent },
 
 ];
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,7 @@
-// app.config.ts
+
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';  // Update to import from app-routing.module.ts
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
